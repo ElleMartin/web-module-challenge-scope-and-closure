b794f10dfc195fb3164d336962bd14e3e50e5d11
Elle Martin-Diogo <elle@Elles-MacBook-Pro.local>
2020-06-04 19:14:12 -0400

    All but the scoreboard task are completed. Scoreboard continues to show error that cb is not a function when it worked fine in the previous problem.

--- index.js
+++ index.js
@@ -28,9 +28,16 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ * In counter1, the value of count is declared on a function-level. However, in counter2, the value of count is declared on a global level.
+ * 
  * 2. Which of the two uses a closure? How can you tell?
  * 
+ * A closure is used in counter1 because count is defined on a function-level - it cannot be accessed outside the scope.
+ * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ * 
+ * Counter1 would be best used if you do not need to be able to reference count thoughout other areas of your code because it will not be accessible outside of the function it is within. 
+ * Counter2 would be best if you will need to reference count in orher areas of the code because it is globally declared in this example.
  *
 */
 
@@ -56,11 +63,19 @@ function counter2() {
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(){
+let score = Math.floor(Math.random()* 3);
+return score;
 }
+console.log(inning());
+console.log(inning());
+console.log(inning());
+console.log(inning());
+console.log(inning());
+console.log(inning());
+console.log(inning());
+console.log(inning());
+console.log(inning());
 
 /* Task 3: finalScore()
 
@@ -73,14 +88,18 @@ finalScore(inning, 9) might return:
   "Home": 11,
   "Away": 5,
 }
-
 */ 
+function finalScore(cb,numInnings) {
+  let homeTeam = 0; //setting starting scores to 0
+  let awayTeam = 0;
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+  for (let i=0; i <= numInnings; i++) { //create our loop to loop through all innings
+    homeTeam += cb();// set our two teams to callbacks - with the "+=", this reads as homeTeam = homeTeam + cb - callbacks are a function, so you add ()
+    awayTeam += cb();
+  }
+  return "Home: " + homeTeam + "\nAway: " + awayTeam;
 }
+console.log(finalScore(inning,9));
 
 /* Task 4: 
 
@@ -103,8 +122,60 @@ and returns the score at each pont in the game, like so:
 
 Final Score: 6 - 10 */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(cb,inningNum) { 
+  let homeTeam = 0;
+  let awayTeam = 0;
+  let numInnings = 9;
+
+   for (let i= 1; i <= numInnings; i++){
+     homeTeam += cb();
+     awayTeam += cb();
+     inningNum += 1;
+   }
+
+   for (let i= 1; i <= numInnings; i++){
+    homeTeam += cb();
+    awayTeam += cb();
+    inningNum += 1;
+   }
+   return ("Inning: " + inningNum + "\nHome: " + homeTeam + "\nAway: " + awayTeam);
+ }
+ console.log(scoreboard());
+
+
+/*1. In your own words, define closure (1-2 sentences).
+
+  Closure refers to defining the different variables and arguements the function needs allowing it to have all the information it requires to do what we are asking it to do. It allows the function a "memory"
+  to refer to in order to know what something we put in means.
+
+2. Study the following code, then answer the questions below.
+```js
+function personalDice(name){
+  return function(){
+      // generate random number between 1 and 6
+    const newRoll = Math.floor(Math.random() * 6);
+    console.log(`${name} rolled a ${newRoll}`)
+  }
 }
 
+const dansRoll = personalDice("Dan");
 
+const zoesRoll = personalDice("Zoe");
+
+
+dansRoll();
+dansRoll();
+```
+
+  a. Where is closure used in this code? How can you tell?
+    
+    The closure is in defining newRoll because it is on the function-level scope - cannot be accessed outside the function.
+
+  b. Compare and contrast calling `dansRoll` the first and second time. What is always the same? What could change?
+    
+    Calling dansRoll each time will always result in an example of a dice roll that Dan made. What changes is the number he rolls because it is a random number generator.
+
+  c. What is the lexical scope of `newRoll`? 
+
+    The lexical scope is Math.floor(Math.random() * 6).
+*/
